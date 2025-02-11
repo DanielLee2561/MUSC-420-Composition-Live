40747fc4a77d9c74ad53246c5994a75a2a7ce31f
Daniel Lee <daniel.j.lee.2561@gmail.com>
2025-02-11 02:04:47 -0800

    Video Keyboard

--- Unity/Assets/Scripts/Note.cs
+++ Unity/Assets/Scripts/Note.cs
@@ -8,7 +8,7 @@ public class Note : MonoBehaviour
     public string oscNote;
 
     // Variable
-    private Dictionary<string, Transform> octaves = new Dictionary<string, Transform>();
+    private Dictionary<int, Transform> keys = new Dictionary<int, Transform>();
 
     void Start()
     {
@@ -16,16 +16,39 @@ public class Note : MonoBehaviour
         if (osc) osc.SetAddressHandler(oscNote, setNote);
 
         // Variable
-        assignOctaves();
+        assignKeys();
     }
 
-    private void assignOctaves()
+    // Given an octave and key, return the pitch.
+    private int keyToPitch(string octave, string key)
     {
-        GameObject[] octaves_obj = GameObject.FindGameObjectsWithTag("Octave");
-        foreach (GameObject octave in octaves_obj)
+        Dictionary<string, int> dict = new Dictionary<string, int>
         {
-            octaves[octave.name] = octave;
-        }
+            { "C", 0},
+            { "C#", 1},
+            { "D", 2},
+            { "D#", 3},
+            { "E", 4},
+            { "F", 5},
+            { "F#", 6},
+            { "G", 7},
+            { "G#", 8},
+            { "A", 9},
+            { "A#", 10},
+            { "B", 11}
+        };
+
+        int pitch_octave = int.Parse(octave.Split('_')[1]);
+        int pitch_key = dict[key.Split('_')[1]];
+        return (pitch_octave + 1) * 12 + pitch_key;
+    }
+
+    private void assignKeys()
+    {
+        GameObject[] octaves = GameObject.FindGameObjectsWithTag("Octave");
+        foreach (GameObject octave in octaves)
+            foreach (Transform key in octave.transform)
+                keys[keyToPitch(octave.name, key.name)] = key;
     }
 
     private void setNote(OscMessage input)
